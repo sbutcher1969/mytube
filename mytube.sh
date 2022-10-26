d6ba4df5c86ce65fd00d042324db0f82edd74336
sbutcher1969 <103070775+sbutcher1969@users.noreply.github.com>
2022-10-26 11:44:38 -0400

    new commit for mytube
    This the Oct 26 ,2022 commit for mytube, I continue to develop and change the interface.  I continue to learn and develop the program .  It should be nearing completion in a couple of weeks.

--- mytube.sh
+++ mytube.sh
@@ -9,14 +9,15 @@
 #
 #                           Scott Butcher
 #      Date: Oct. 2022
-#      System: Arcolinux Arch 5.19.13
+#      System: MX Linux & Termux
 #
 #      **********************************
 #      *  Check for CLI search attempt  *
 #      **********************************
-echo " ">cspace
+echo " ">.cspace
+#set -x                                #  functrace
 if [[ -n $* ]];
-then echo $*>.lsearch; cat myconfigs.txt cspace .lsearch>.search ; ytfzf $(cat ./.search); exit
+then echo $*>.lsearch; cat .fconfig .cspace .lsearch>.search ; ytfzf $(cat ./.search); exit
 fi
 
 #      *********************************
@@ -24,55 +25,63 @@ fi
 #      *********************************
 
 function clear_vars(){
-    echo "">options
-    echo "">teminfo
-    echo "">csearch
-    echo "">sfinal
-    echo "">configs
-    echo "">dchoice
-    main
+    echo "">.options
+    echo "">.teminfo
+    echo "">.csearch
+    echo "">.sfinal
+    echo "">.configs
+    echo "">.dchoice
+   return 
 }
-
 #      *********************************
-#      *      Add search options       *
+#      *      Add search .options       *
 #      *********************************
 
-function add_options(){
-    dialog --single-quoted --notags --colors --checklist "\ZbOPTIONS\ZB" 18 40 10 ' -t' "thumbs" off ' -l' "loop" off ' -r' "random" off ' -a' "auto play" off ' -H' "history" off ' -D' "dMenu" off ' -A' "Select all" off ' -m' "Audio only" off ' -f' "formats available" off ' --sort' "Sort by Date (most recent)" off ' -d' "Download" off 2>options
-  ## if [[ grep " -d" ./options ]]options then dchoice>>options
-    sed -i s/"'"/""/g  options
-    main
-};
+function add_.options(){
+    dialog --single-quoted --notags --colors --checklist "\Zb.options\ZB" 18 40 10 ' -t' "thumbs" off ' -l' "loop" off ' -r' "random" off ' -a' "auto play" off ' -H' "history" off ' -D' "dMenu" off ' -A' "Select all" off ' -m' "Audio only" off ' -f' "formats available" off ' --sort' "Sort by Date (most recent)" off ' -d' "Download" off 2>.options
+  ## if [[ grep " -d" ./.options ]].options then.dchoice>>.options
+    sed -i s/"'"/""/g  .options
+    return
+}
 
 #      *********************************
 #      *   Add Video/Audio features    *
 #      *********************************
 
 function features_str(){
-    dialog --single-quoted --notags --colors --checklist "\ZbFEATURES\ZB" 12 40 6 'hd,' "HD" off '3d,' "3-D" off '4k,' "4k" off '360,' "360" off 'hdr,' "HDR" off 'creative_commons,' "Creative Commons," off 2>configs
-    main
+	echo "">>.configs
+	echo "">>.fconfigs
+	echo " --features=">>.feats
+    dialog --single-quoted --notags --colors --checklist "\ZbFEATURES\ZB" 12 40 6 'hd,' "HD" off '3d,' "3-D" off '4k,' "4k" off '360,' "360" off 'hdr,' "HDR" off 'creative_commons,' "Creative Commons," off 2>.fconfigs
+    if [[ -n .fconfigs ]];
+    then cat .feats .fconfigs > .configs
+    return
 
-};
+}
 
 #      *********************************
-#      *    Add Information options    *
+#      *    Add Information .options    *
 #      *********************************
 
 info_str(){
-    echo " -I">teminfo
-    dialog --no-lines --colors --menu "\ZbINFO\ZB" 12 40 5 " L" "Link" " VJ" "video-JSON" " J" "JSON" " F" "Format" " R" " Raw" 2>&1>teminfo
-    main
-};
+    echo " -I">.teminfo
+    infochoice=$(dialog --no-lines --colors --menu "\ZbINFO\ZB" 12 40 5 " L" "Link" " VJ" "video-JSON" " J" "JSON" " F" "Format" " R" " Raw" 2>&1>/dev/tty)
+    echo $infochoice>>.teminfo
+    return
+}
 
 #      ********************************
 #      *  CLI & Search Configuration  *
 #      ********************************
 
 conf_str(){
-    dialog --notags --single-quoted --colors --visit-items --buildlist "\ZbCONFIGURATION\ZB" 15 25 11 ' -t' "thumbs" unselected ' -l' "loop" unselected ' -r' "random" unselected ' -a' "auto play" unselected ' -H' "history" unselected ' -D' "dMenu" unselected ' -A' "Select all" unselected ' -m' "Audio only" unselected ' -f' "formats available" unselected ' --sort' "Sort by Date (most recent)" unselected ' -d' "Download" unselected 2>myconfigs.txt
-    sed -i s/"'"/""/g  myconfigs.txt
-    main
-};
+    dialog --notags --single-quoted --colors --visit-items --buildlist "\ZbCONFIGURATION\ZB" 15 25 11 ' -t' "thumbs" unselected ' -l' "loop" unselected ' -r' "random" unselected ' -a' "auto play" unselected ' -H' "history" unselected ' -D' "dMenu" unselected ' -A' "Select all" unselected ' -m' "Audio only" unselected ' -f' "formats available" unselected ' --sort' "Sort by Date (most recent)" unselected ' -d' "Download" unselected 2>.configs.txt
+    sed -i s/"'"/""/g  .configs.txt
+    dialog --single-quoted --notags --colors --checklist "\ZbFEATURES\ZB" 12 40 6 ' hd,' "HD" off ' 3d,' "3-D" off ' 4k,' "4k" off ' 360,' "360" off ' hdr,' "HDR" off ' creative_commons,' "Creative Commons," off 2>.confi
+    cat .configs.txt .confi > .fconfig
+
+    return
+}
 
 #      ********************************
 #      *    Add Subscriptions/List    *
@@ -80,7 +89,7 @@ conf_str(){
 
 subs_str(){
 
-    main
+    return
 };
 
 #      ********************************
@@ -88,16 +97,16 @@ subs_str(){
 #      ********************************
 
 function search_item (){
-    dialog --inputbox "Search Item(s):" 8 25 2> csearch                         # input box to ask for search string (csearch)
+    dialog --inputbox "Search Item(s):" 8 25 2> .csearch                         # input box to ask for search string (.csearch)
 
     clear
 
-    cat cspace options cspace configs teminfo cspace dchoice csearch > sfinal   # assemble the query string for ytfzf
-    ytfzf $(cat ./sfinal)
-    if [[ -N teminfo ]];
+    cat .cspace .options .cspace .configs .teminfo .cspace.dchoice .csearch > .sfinal   # assemble the query string for ytfzf
+    ytfzf $(cat ./.sfinal)
+    if [[ -N .teminfo ]];
     then echo "enter to continue" ; read newlin ; clear_vars
     fi
-};
+}
 
 #      ********************************
 #      *      Download Path/File      *
@@ -105,11 +114,11 @@ function search_item (){
 function download_video(){
 
     dialog --stdout --title "Video Download Path" --dselect $HOME/ 14 48 2>&1>dschoice
-    echo " --ytdl-path=">dpath
-    cat dpath dschoice cspace >>dchoice
-    main
+    echo " --ytdl-path=">.dpath
+    cat .dpath dschoice .cspace >.dchoice
+    return
 
-};
+}
 
 #      *******************************
 #      #    Main Menu / Main loop    *
@@ -119,19 +128,19 @@ main(){
 
     choice=""
 
-    choice=$(dialog --menu "SELECT" 15 50 6 "1" "Search Videos" "2" "Search Options" "3" "Features" "4"  "Information" "5" "Configuration" "6" "Subscriptions" "7" "Set Download Path" "8" "Quit" 2>&1>/dev/tty)
+    choice=$(dialog --menu "SELECT" 15 50 6 "1" "Search Videos" "2" "Search .options" "3" "Features" "4"  "Information" "5" "Configuration" "6" "Subscriptions" "7" "Set Download Path" "8" "Quit" 2>&1>/dev/tty)
 
 case $choice in
     1) search_item;;
-    2) add_options;;
-    3) features_str;;
-    4) info_str;;
-    5) conf_str;;
-    6) subs_str ;;
-    7) download_video;;
-    8) exit;;
+    2) add_.options;main;;
+    3) features_str;main;;
+    4) info_str;main;;
+    5) conf_str;main;;
+    6) subs_str;main;;
+    7) download_video;main;;
+    8) clear;exit;;
 esac
-};
+}
 main
 
 clear_vars
@@ -145,3 +154,4 @@ clear_vars
 #       - get subscriptions working
 #       - setup configuration of downloads/delete choice
 #       - make video vault section
+#   use declare to functions for return to work
